2381f7c5a0f7933605af77792500a0684074efe9
Shoichiro <lexion13@gmail.com>
2016-01-22 07:39:14 +0000

    microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,4 +56,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  # You can have the root of your site routed with "root"
+
 end
